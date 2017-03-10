8b44f28f0e4cbb6767207515aa5010ef681761a0
Kirill <timofeev.kirill.a@gmail.com>
2017-03-10 21:31:29 +0300

    seems to be done

--- actors/src/main/scala/im/kirillt/sd/Main.scala
+++ actors/src/main/scala/im/kirillt/sd/Main.scala
@@ -1,15 +1,25 @@
 package im.kirillt.sd
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
 import scala.concurrent.duration._
 /**
   * Created by kirill on 09.03.17.
   */
 object Main extends App {
-  val system = ActorSystem("Searcher")
-  val master = system.actorOf(MasterActor.props(10 seconds))
-  master ! SearchRequest("Blah")
-  Thread.sleep(10000)
-  system.stop(master)
-  //sys.exit(1)
+  val system = ActorSystem("SearcherApp")
+  val master = system.actorOf(MasterActor.props(2 seconds))
+  val local = system.actorOf(Props(new UserIOActor()))
+  master.tell(SearchRequest("Blah"), local)
+}
+
+class UserIOActor extends Actor {
+  override def receive: Receive = {
+    case "START" =>
+    case MasterActorResponse(links) =>
+      println("get answer from master:")
+      links foreach (it  => println(s"${it._1}: ${it._2}"))
+    case _ =>
+  }
 }
--- actors/src/main/scala/im/kirillt/sd/MasterActor.scala
+++ actors/src/main/scala/im/kirillt/sd/MasterActor.scala
@@ -5,21 +5,28 @@ import akka.pattern.{ask, pipe}
 import akka.util.Timeout
 
 import scala.concurrent.Future
+import scala.util.{Success, Failure}
 
 /**
   * Created by kirill on 09.03.17.
   */
 class MasterActor(engineResponseTimeout: Timeout) extends Actor with ActorLogging {
+
   import scala.concurrent.ExecutionContext.Implicits.global
+
   implicit val timeout: Timeout = engineResponseTimeout
+
   override def receive: Receive = {
     case request@SearchRequest(query) =>
       log.info(s"get $query")
       val children = SearchEngine.values.map(e => context.actorOf(ChildActor.props(e))).toList
-      val result = children map ( c => c ? request) map (f => f.mapTo[ChildActorResponse])
+      val result = children map (c => c ? request) map (f => f.mapTo[ChildActorResponse])
       val resultList = Future.sequence(result).map(list => list.map(charesp => (charesp.engine, charesp.links)).toMap)
       val resp = resultList.map(MasterActorResponse)
-
+      resp.onComplete {
+        case Success(s) => context stop self
+        case Failure(e) => context stop self
+      }
       pipe(resp) to sender()
     case _ =>
       log.info("unknown message")
